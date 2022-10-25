854c3f80e49ae092d72ddad0c88ffa05c0b36a55
Paul Leszyk <pleszyk@gmail.com>
2022-10-25 14:24:40 -0400

    user auth

--- client/src/store.js
+++ client/src/store.js
@@ -4,13 +4,19 @@ import {
   productDetailsReducer,
 } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducer';
+import { userLoginReducer } from './reducers/userReducers';
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : [];
 
+const userInfoItemsFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null;
+
 const preloadedState = {
-  cart: { cartItems: cartItemsFromStorage }
+  cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoItemsFromStorage },
 };
 
 const store = configureStore({
@@ -18,9 +24,9 @@ const store = configureStore({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
+    userLogin: userLoginReducer,
   },
-  preloadedState
+  preloadedState,
 });
-
 
 export default store;
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,29 +1,10 @@
 import express from 'express'
-import AsyncHandler from 'express-async-handler'
-import Product from '../models/productModel.js'
+import { getProducts, getProductById } from '../controllers/productController.js'
 
 const router = express.Router()
 
-// @desc Fetch all products
-// @route GET api/products
-// @access Public
-router.get('/', AsyncHandler(async (req, res) => {
-    const products = await Product.find({})
-    res.json(products)
-}))
+router.route('/').get(getProducts)
+router.route('/:id').get(getProductById)
 
-// @desc Fetch a product
-// @route GET api/products/:id
-// @access Public
-router.get('/:id', AsyncHandler(async(req, res) => {
-    const product = await Product.findById(req.params.id)
-
-    if(product) {
-        res.json(product)
-    } else {
-        res.status(404)
-        throw new Error('Product not found')
-    }
-}))
 
 export default router
