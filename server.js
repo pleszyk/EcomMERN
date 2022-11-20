6b22a7340e3836a207211aa5f28a6756131d6857
Paul Leszyk <pleszyk@gmail.com>
2022-11-20 09:37:20 -0500

    deploy

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
@@ -19,6 +20,14 @@ app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
 
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
+
+const __dirname = path.resolve()
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '/client/build')))
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
 
 const PORT = process.env.PORT || 4000
 
